16f636e26a9918fc2382a951cdf1ad33cb4164ba
Mariia <shymanskam524@gmail.com>
2025-04-02 12:03:30 +0200

    Нормалізація кінців рядків

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,10 +1,5 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-
-
 class Config:
-    DB_URL = "postgresql+asyncpg://postgres:fE512@localhost:5432/contacts"
-
+    DB_URL = "postgresql+asyncpg://postgres:fe312@localhost:5432/contacts"
 
 config = Config
 
